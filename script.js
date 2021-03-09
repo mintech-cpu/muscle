980ada5cc629204e64b844e4d4382aefc7f3cca2
mintech-cpu <78586667+mintech-cpu@users.noreply.github.com>
2021-03-09 16:30:00 +0900

    Add files via upload
    initial commit

---
+++ script.js
@@ -0,0 +1,92 @@
+$(function() {
+    $('.slider').slick({
+        autoplay: true,//自動的に動き出すか。初期値はfalse。
+        infinite: true,//スライドをループさせるかどうか。初期値はtrue。
+        slidesToShow: 3,//スライドを画面に3枚見せる
+        slidesToScroll: 3,//1回のスクロールで3枚の写真を移動して見せる
+        prevArrow: '<div class="slick-prev"></div>',//矢印部分PreviewのHTMLを変更
+        nextArrow: '<div class="slick-next"></div>',//矢印部分NextのHTMLを変更
+        dots: true,//下部ドットナビゲーションの表示
+        responsive: [
+            {
+            breakpoint: 769,//モニターの横幅が769px以下の見せ方
+            settings: {
+                slidesToShow: 2,//スライドを画面に2枚見せる
+                slidesToScroll: 2,//1回のスクロールで2枚の写真を移動して見せる
+            }
+        },
+        {
+            breakpoint: 426,//モニターの横幅が426px以下の見せ方
+            settings: {
+                slidesToShow: 1,//スライドを画面に1枚見せる
+                slidesToScroll: 1,//1回のスクロールで1枚の写真を移動して見せる
+            }
+        }
+    ]
+    });
+    
+    
+    
+});
+
+
+//任意のタブにURLからリンクするための設定
+function GethashID (hashIDName){
+	if(hashIDName){
+		//タブ設定
+		$('.tab li').find('a').each(function() { 
+			var idName = $(this).attr('href'); 
+			if(idName == hashIDName){ 
+				var parentElm = $(this).parent(); 
+				$('.tab li').removeClass("active"); 
+				$(parentElm).addClass("active"); 
+				//表示させるエリア設定
+				$(".area").removeClass("is-active");
+				$(hashIDName).addClass("is-active"); 
+			}
+		});
+	}
+}
+
+//タブをクリックしたら
+$('.tab a').on('click', function() {
+	var idName = $(this).attr('href'); 
+	GethashID (idName);
+	return false;
+});
+
+
+// 上記の動きをページが読み込まれたらすぐに動かす
+$(window).on('load', function () {
+    $('.tab li:first-of-type').addClass("active"); 
+    $('.area:first-of-type').addClass("is-active");
+	var hashName = location.hash;
+	GethashID (hashName);
+});
+
+
+//アコーディオンをクリックした時の動作
+$('.item_title').on('click', function() {//タイトル要素をクリックしたら
+	$('.box').slideUp(500);//クラス名.boxがついたすべてのアコーディオンを閉じる
+    
+	var findElm = $(this).next(".box");//タイトル直後のアコーディオンを行うエリアを取得
+    
+	if($(this).hasClass('close')){//タイトル要素にクラス名closeがあれば
+		$(this).removeClass('close');//クラス名を除去    
+	}else{//それ以外は
+		$('.close').removeClass('close'); //クラス名closeを全て除去した後
+		$(this).addClass('close');//クリックしたタイトルにクラス名closeを付与し
+		$(findElm).slideDown(500);//アコーディオンを開く
+	}
+});
+
+//ページが読み込まれた際にopenクラスをつけ、openがついていたら開く動作※不必要なら下記全て削除
+$(window).on('load', function(){
+	$('.accordion-area li:first-of-type section').addClass("open"); //accordion-areaのはじめのliにあるsectionにopenクラスを追加
+	$(".open").each(function(index, element){	//openクラスを取得
+		var Title =$(element).children('.item_title');	//openクラスの子要素のtitleクラスを取得
+		$(Title).addClass('close');				///タイトルにクラス名closeを付与し
+		var Box =$(element).children('.box');	//openクラスの子要素boxクラスを取得
+		$(Box).slideDown(500);					//アコーディオンを開く
+	});
+});
